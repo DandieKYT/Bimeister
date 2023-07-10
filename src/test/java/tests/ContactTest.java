b2f1871a44f4fd786689056ad45b1c2d1dc7c82f
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-07-10 21:01:47 +0300

    4 push

--- src/test/java/tests/ContactTest.java
+++ src/test/java/tests/ContactTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 public class ContactTest extends TestBase{
     @Test
     public void checkContacts(){
-
+    basePage.openPage();
+    contactPage.contactPageOpen();
+    contactPage.checkAddress();
+    contactPage.checkNumberPhone();
+    contactPage.checkEmail();
     }
 }
